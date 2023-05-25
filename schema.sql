c80f46ec76ad71f54b7df7b9cf83ca40147d933c
Tammam Tautou <tautoutammam@gmail.com>
2023-05-25 11:06:23 -0700

    ETL with compy statement

--- schema.sql
+++ schema.sql
@@ -2,11 +2,9 @@
 -- to istall PostgreSQL visit: https://www.postgresql.org/docs/
 -- Start by entering the following on the command line:
 -- psql postgres
--- CREATE ROLE addname WITH LOGIN PASSWORD 'quoted password'
--- ALTER ROLE addname CREATEDB;
 -- CREATE DATABASE databasename;
 
--- To run a SQL file (schema) in PostgreSQL from the shell run the following command in your terminal.
+-- To run a SQL file (schema) in PostgreSQL from the shell navegate into the dr that has the schema and  run the following command in your terminal.
 -- psql -U atelier -d atelier -a -f /path/to/your/schema.sql
 
 DROP TABLE IF EXISTS characteristic_reviews;
@@ -17,17 +15,22 @@ DROP TABLE IF EXISTS products;
 
 
 CREATE TABLE IF NOT EXISTS products (
-  product_id SERIAL PRIMARY KEY
+  product_id SERIAL PRIMARY KEY,
+  name TEXT,
+  slogan TEXT,
+  description TEXT,
+  category TEXT,
+  default_price INT
 );
 
 CREATE TABLE IF NOT EXISTS reviews (
   review_id SERIAL PRIMARY KEY,
   rating INT DEFAULT 0,
-  date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+  date TEXT,
   summary VARCHAR(255),
   body TEXT,
-  recommend BOOLEAN,
-  reported BOOLEAN,
+  recommend BOOL NOT NULL,
+  reported BOOL NOT NULL,
   reviewer_name VARCHAR(50),
   reviewer_email VARCHAR(100),
   response TEXT DEFAULT NULL,
@@ -42,7 +45,7 @@ CREATE TABLE IF NOT EXISTS reviews_photos (
 );
 
 CREATE TABLE IF NOT EXISTS characteristics (
-  characteristics_id SERIAL PRIMARY KEY,
+  characteristic_id SERIAL PRIMARY KEY,
   name VARCHAR(50),
   product_id INTEGER REFERENCES products(product_id)
 );
@@ -50,8 +53,33 @@ CREATE TABLE IF NOT EXISTS characteristics (
 CREATE TABLE IF NOT EXISTS characteristic_reviews (
   characteristic_reviews_id SERIAL PRIMARY KEY,
   value INTEGER,
-  characteristics_id INTEGER REFERENCES characteristics(characteristics_id),
+  characteristic_id INTEGER REFERENCES characteristics(characteristic_id),
   review_id INTEGER REFERENCES reviews(review_id)
 );
 
 
+-- COPY reviews(review_id, product_id,rating,date, summary, body, recommend, reported,reviewer_name, reviewer_email, response, helpfulness)
+-- FROM '/Users/tammamtautou/Downloads/reviews/reviews.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+
+
+-- COPY products(product_id, name, slogan, description, category,default_price)
+-- FROM '/Users/tammamtautou/Downloads/characteristics/product.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+
+-- COPY reviews_photos(reviews_photos_id, review_id, url)
+-- FROM '/Users/tammamtautou/Downloads/reviews_photos/reviews_photos.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+
+-- COPY characteristics(characteristic_id, product_id, name)
+-- FROM '/Users/tammamtautou/Downloads/characteristics/characteristics.csv'
+-- DELIMITER ','
+-- CSV HEADER;
+
+-- COPY characteristic_reviews(characteristic_reviews_id, characteristic_id, review_id, value)
+-- FROM '/Users/tammamtautou/Downloads/characteristic_reviews/characteristic_reviews.csv'
+-- DELIMITER ','
+-- CSV HEADER;
