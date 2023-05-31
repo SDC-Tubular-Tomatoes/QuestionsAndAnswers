45e53453f50076955ccf98b6948477a4d2cc58c3
Tammam Tautou <tautoutammam@gmail.com>
2023-05-31 15:10:59 -0700

    change location

--- server/controllers.js
+++ server/controllers.js
@@ -4,12 +4,12 @@ const models = require('./models');
 exports.getListReviews = (req, res) => {
   models
     .listReviews(
+      req.query.product_id,
       req.query.page,
       req.query.count,
       req.query.sort,
-      req.query.product_id,
     )
-    .then((result) => (res.status(200).send(result)))
+    .then((result) => (res.status(200).send(result[0])))
     .catch((err) => {
       console.log('CONTROLLER REVIEWS GET LIST REVIEWS', err);
       res.sendStatus(505);
@@ -19,7 +19,7 @@ exports.getListReviews = (req, res) => {
 exports.getReviewsMetadata = (req, res) => {
   models
     .metadata(req.query.product_id)
-    .then((result) => (res.status(200).send(result)))
+    .then((result) => (res.status(200).send(result[0])))
     .catch((err) => {
       console.log('CONTROLLER REVIEWS GET REVIEWS METADATA', err);
       res.sendStatus(505);
@@ -46,22 +46,28 @@ exports.getReviewsMetadata = (req, res) => {
 //     });
 // };
 
-// exports.putReviewAsHelpful = (req, res) => {
-//   models.reviews
-//     .markReviewAsHelpful(req.params.review_id)
-//     .then(() => res.sendStatus(204))
-//     .catch((err) => {
-//       console.log('CONTROLLER REVIEWS PUT REVIEW', err);
-//       res.sendStatus(505);
-//     });
-// };
+exports.putReviewAsHelpful = (req, res) => {
+  models
+    .markReviewAsHelpful(req.params.review_id)
+    .then(() => {
+      res.sendStatus(204);
+      models.refresh_reviews();
+    })
+    .catch((err) => {
+      console.log('CONTROLLER REVIEWS PUT REVIEW', err);
+      res.sendStatus(505);
+    });
+};
 
-// exports.putReportReview = (req, res) => {
-//   models.reviews
-//     .reportReview(req.params.review_id)
-//     .then(() => res.sendStatus(204))
-//     .catch((err) => {
-//       console.log('CONTROLLER REVIEWS PUT REVIEW', err);
-//       res.sendStatus(505);
-//     });
-// };
+exports.putReportReview = (req, res) => {
+  models
+    .reportReview(req.params.review_id)
+    .then(() => {
+      res.sendStatus(204);
+      models.refresh_reviews();
+    })
+    .catch((err) => {
+      console.log('CONTROLLER REVIEWS PUT REVIEW', err);
+      res.sendStatus(505);
+    });
+};
