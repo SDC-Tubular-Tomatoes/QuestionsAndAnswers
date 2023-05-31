e861aa8be8667ce4ead5df57cbe2d306e74e9f9c
Tammam Tautou <tautoutammam@gmail.com>
2023-05-31 15:11:31 -0700

    delete middleware it slows the api

--- server/index.js
+++ server/index.js
@@ -2,19 +2,13 @@
 // import and configure dotenv, import other lobraries and modules
 require('dotenv').config();
 const express = require('express');
-const morgan = require('morgan');
-const path = require('path');
 const router = require('./routes');
-// const logger = require('./logger');
 
 // initializes a new Express application
 const app = express();
 
 // middelwares
-app.use(express.static(path.join(__dirname, '../client/dist')));
 app.use(express.json());
-app.use(morgan('dev'));
-// app.use(logger);
 
 // Create route
 app.use('/api', router);
