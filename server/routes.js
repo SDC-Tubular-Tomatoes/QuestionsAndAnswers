c5eb84c0c1d13afdb4dd5e28b4014df470368678
Tammam Tautou <tautoutammam@gmail.com>
2023-05-31 15:12:09 -0700

    add routes

--- server/routes.js
+++ server/routes.js
@@ -6,10 +6,9 @@ const router = express.Router();
 
 // Reviews
 router.get('/reviews', controllers.getListReviews);
-
 router.get('/reviews/meta', controllers.getReviewsMetadata);
 // router.post('/reviews', controllers.postReview);
-// router.put('/reviews/:review_id/helpful', controllers.putReviewAsHelpful);
-// router.put('/reviews/:review_id/report', controllers.putReportReview);
+router.put('/reviews/:review_id/helpful', controllers.putReviewAsHelpful);
+router.put('/reviews/:review_id/report', controllers.putReportReview);
 
 module.exports = router;
