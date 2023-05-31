befb2dcd56da7f07ac90f5f1c9260b7527f540b0
Tammam Tautou <tautoutammam@gmail.com>
2023-05-30 22:30:39 -0700

    add k6 stress test

---
+++ k6/k6_meta.js
@@ -0,0 +1,18 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable import/no-unresolved */
+/* eslint-disable camelcase */
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  stages: [
+    { duration: '1s', target: 1100 },
+    { duration: '28s', target: 1100 },
+    { duration: '1s', target: 1100 },
+  ],
+};
+
+export default function () {
+  const random_product_id = Math.floor(900000 + Math.random() * 100000);
+  const res = http.get(http.url`http://localhost:3000/api/reviews/meta/?product_id=${random_product_id}`);
+}
